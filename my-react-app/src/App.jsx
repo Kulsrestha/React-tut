886b3076db753a5a704e86b3f12b26c62ee2e071
Kulsrestha <kjkulsrestha2002@gamil.com>
2025-12-25 19:40:39 +0530

    feat: add header component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./components/header/header";
 
 function App() {
+  return (
+    <Header></Header>
+  );
 }
 
 export default App
---
+++ my-react-app/src/components/header/header.lazy.js
@@ -0,0 +1,11 @@
+import React, { lazy, Suspense } from 'react';
+
+const Lazyheader = lazy(() => import('./header'));
+
+const header = props => (
+  <Suspense fallback={null}>
+    <Lazyheader {...props} />
+  </Suspense>
+);
+
+export default header;
